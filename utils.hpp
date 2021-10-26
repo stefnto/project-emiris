433ad35fb170591fb36c47f68f1f830e38ed5539
sliiime <kostis-p@windowslive.com>
2021-10-26 17:31:12 +0300

    added a util source and header file

---
+++ utils.hpp
@@ -0,0 +1,11 @@
+#ifndef __LSH__UTILS__
+#define __LSH__UTILS__
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+int mod(int x, int y);
+double EuclidianDistance(std::vector<int> a, std::vector<int> b);
+
+#endif
