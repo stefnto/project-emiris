169dcc6f64af47891d471590be4831dbe79ff7e1
sliiime <kostis-p@windowslive.com>
2021-10-26 17:31:12 +0300

    added a util source and header file

---
+++ utils.cpp
@@ -0,0 +1,20 @@
+#include "utils.hpp"
+
+int mod(int x, int y){
+    if (x < 0)
+        return y - ((-x) % y);
+    return x % y;
+}
+
+double EuclidianDistance(std::vector<double> a, std::vector<double> b){
+    if (a.size() != b.size()) return 0;
+
+    double sum = 0;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        sum += pow((a.at(i) - b.at(i)), 2);
+    }
+
+    return sqrt(sum);
+}
